63aba6a292c4b92e57fa71ec4cfad38e28d87068
dougiehawes <dougiehawes@hotmail.com>
2026-02-19 11:03:33 +0000

    first commit

--- server.js
+++ server.js
@@ -5,18 +5,17 @@ import express from "express";
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-// middleware
+// middleware — ORDER MATTERS
 app.use(cors());
+app.options("*", cors());
 app.use(express.json());
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello World!" });
 });
 
-// start listen
 app.listen(PORT, () => {
   console.log(`server running on port:${PORT}`);
 });
