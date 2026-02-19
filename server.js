797c42c554c9909c062ac952b8bb71caabb35561
dougiehawes <dougiehawes@hotmail.com>
2026-02-19 11:00:26 +0000

    first commit

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // middleware
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello World!" });
