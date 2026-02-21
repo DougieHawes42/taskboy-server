24f00934aa50d6d92f52cb6b4558d77622cdfe37
dougiehawes <dougiehawes@hotmail.com>
2026-02-21 10:27:45 +0000

    fixing backend connection

--- server.js
+++ server.js
@@ -8,9 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // middleware — ORDER MATTERS
-app.use(cors());
-app.options("*", cors());
 app.use(express.json());
+app.use(cors());
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello World!" });
